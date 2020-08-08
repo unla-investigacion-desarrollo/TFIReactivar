4d920412b8f1739ac881bb0447fa6d0ee55f941a
Matias Torsello <matiastorsello@gmail.com>
2020-08-08 19:18:47 -0300

    Test 4 heroku

--- src/main/java/com/unla/alimentar/AlimentarApplication.java
+++ src/main/java/com/unla/alimentar/AlimentarApplication.java
@@ -2,13 +2,14 @@ package com.unla.alimentar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class AlimentarApplication {
+public class AlimentarApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlimentarApplication.class, args);
