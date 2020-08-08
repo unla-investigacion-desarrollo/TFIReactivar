6bf29c2f15e942740627aac3c7a24cb2527d6329
Matias Torsello <matiastorsello@gmail.com>
2020-08-08 16:35:31 -0300

    v0.0.1 - Traer rubro por id

--- src/main/java/com/unla/alimentar/AlimentarApplication.java
+++ src/main/java/com/unla/alimentar/AlimentarApplication.java
@@ -3,13 +3,9 @@ package com.unla.alimentar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.unla.alimentar.security.JWTAuthorizationFilter;
 
 @SpringBootApplication
 public class AlimentarApplication {
@@ -24,11 +20,7 @@ public class AlimentarApplication {
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable()
-				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
-				.authorizeRequests()
-				.antMatchers(HttpMethod.POST, "/usuario").permitAll()
-				.anyRequest().authenticated();
+			http.csrf().disable();
 		}
 	}
 }
--- src/main/java/com/unla/alimentar/controler/UsuarioController.java
+++ src/main/java/com/unla/alimentar/controler/UsuarioController.java
@@ -3,15 +3,11 @@
  */
 package com.unla.alimentar.controler;
 
-import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.unla.alimentar.modelo.Usuario;
 import com.unla.alimentar.service.UsuarioService;
-
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 
 /**
  * @author Matias
@@ -47,9 +40,7 @@ public class UsuarioController {
 		if(!pwd.equals(user.getPassword())){
 			//Lanzar Exception
 		}
-		
-		user.setToken(getJWTToken(username));
-		
+				
 		return new ResponseEntity<>(user, HttpStatus.OK);
 
 	}
@@ -59,17 +50,4 @@ public class UsuarioController {
 		return new ResponseEntity<>(usuarioService.traerTodos(), HttpStatus.OK);
 	}
 
-	private String getJWTToken(String username) {
-		String secretKey = "mySecretKey";
-		List<GrantedAuthority> grantedAuthorities = AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER");
-
-		String token = Jwts.builder().setId("softtekJWT").setSubject(username)
-				.claim("authorities",
-						grantedAuthorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
-				.setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() + 600000))
-				.signWith(SignatureAlgorithm.HS512, secretKey.getBytes()).compact();
-
-		return token;
-	}
 }
