295bd6fe8774dba84a12b694c1dbb7b1662d419e
Matias Torsello <matiastorsello@gmail.com>
2020-08-08 19:21:51 -0300

    Revert "Test 4 heroku"
    This reverts commit 4d920412b8f1739ac881bb0447fa6d0ee55f941a.

--- src/main/java/com/unla/alimentar/AlimentarApplication.java
+++ src/main/java/com/unla/alimentar/AlimentarApplication.java
@@ -2,14 +2,13 @@ package com.unla.alimentar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class AlimentarApplication extends SpringBootServletInitializer{
+public class AlimentarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlimentarApplication.class, args);
