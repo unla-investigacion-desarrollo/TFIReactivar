f6035a48febf3eb932c4db80dd26430d5ae57d1f
Matias Torsello <matiastorsello@gmail.com>
2020-08-26 17:54:04 -0300

    Logger configurado, falta utilizar variable para la ruta de logback

---
+++ src/main/java/com/unla/reactivar/config/RootConfig.java
@@ -0,0 +1,10 @@
+package com.unla.reactivar.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class RootConfig {
+
+}
