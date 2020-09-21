7166a22e76022f4c7b0889d8e133c9da7a29e839
Joy Tabella <joy.tabella@gmail.com>
2020-09-21 15:03:17 -0300

    Se elimina SP innecesario y agrego Controller RubroKm

--- src/main/resources/Stored_Procedure/sp_mostrarPorRubro.sql
+++
@@ -1,8 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_mostrarPorRubro`(IN id_rubro bigint(20))
-BEGIN
-	select *
-    from emprendimiento AS e
-    join rubro AS r ON e.id_rubro=r.id_rubro
-    where e.id_rubro=id_rubro;
-
-END
