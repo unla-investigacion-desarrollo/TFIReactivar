ba3e75f8ebc07f283c51688beda05fa97ee5bd4e
Matias Torsello <matiastorsello@gmail.com>
2020-09-29 19:49:33 -0300

    Agregamos Firebase Cloud Notificactions

--- src/main/java/com/unla/reactivar/config/RootConfig.java
+++ src/main/java/com/unla/reactivar/config/RootConfig.java
@@ -7,10 +7,11 @@ import org.springframework.context.annotation.PropertySource;
 @ComponentScan(basePackages = { "com.unla.reactivar", "com.unla.reactivar.config", "com.unla.reactivar.controllers",
 		"com.unla.reactivar.exceptions", "com.unla.reactivar.exceptions.handler",
 		"com.unla.reactivar.exceptions.models", "com.unla.reactivar.models", "com.unla.reactivar.repositories",
-		"com.unla.reactivar.security", "com.unla.reactivar.services", "com.unla.reactivar.task" })
+		"com.unla.reactivar.security", "com.unla.reactivar.services", "com.unla.reactivar.task", "com.unla.reactivar.firebase"})
 @Configuration
 @PropertySource("file:${root.path}/app-directory/cfg/security/config.properties")
 @PropertySource("file:${root.path}/app-directory/cfg/mail/config.properties")
+@PropertySource("file:${root.path}/app-directory/cfg/firebase/config.properties")
 public class RootConfig {
 
 }
